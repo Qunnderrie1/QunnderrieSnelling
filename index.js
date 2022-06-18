062a9757f5a222444b680c42a7cb5dadbc234c59
Qunnderrie Snelling <qunnderrie@gmail.com>
2022-06-18 02:11:30 -0400

    made changes

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const hamburger = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".nav-menu")
 const submitBtn = document.querySelector(".btn")
+const userName = document.getElementById("name")
+const userEmail = document.getElementById("email")
+const userMessage = document.getElementById("userMessage")
+
 
 hamburger.addEventListener('click', () =>{
     hamburger.classList.toggle('active')
@@ -8,9 +12,14 @@ hamburger.addEventListener('click', () =>{
 })
 
 submitBtn.addEventListener('click', ()=>{
-
     
-    alert("Your message has been sent!")
+    if (userName.value == ""){
+        alert("Please a value.")
+    }else if(userEmail.value == ""){
+        alert("Please enter a value")
+    }else if(userMessage.value == ""){
+        alert("Please enter a value")
+    }
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", ()=>{
